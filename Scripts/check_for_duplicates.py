7b2771fcbbb258a9d152350d7f41b7041f6c341d
marcus.bitzl@bsb-muenchen,de <marcus.bitzl@bsb-muenchen,de>
2014-08-01 13:30:33 +0200

    Initial commit.

---
+++ Scripts/check_for_duplicates.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+from __future__ import print_function
+import argparse
+import sys
+
+class Info:
+	def __init__(self, line):
+		parts = line.split()
+		self.timestamp = parts[0]
+		self.response_code = int(parts[1])
+		self.size = parts[2]
+		self.url = parts[3]
+		self.breadcrumb = parts[4]
+		self.referrer = parts[5]
+		self.mime = parts[6]
+		self.worker = parts[7]
+		self.fetch_timestamp = parts[8]
+		self.hash = parts[9]
+		self.source_tag = parts[10]
+		self.annotations = parts[11]
+
+
+def omit_this(info, response_code):
+	return response_code is not None and not response_code == info.response_code
+
+
+# def main2(crawl_log, response_code, write=print):
+# 	print('Parse log file for comparison ("%s")...' % crawl_log)
+
+# 	with open(crawl_log, 'r') as src:
+# 		resources = extract_resources(crawl_log, response_code)
+
+# 	print('Write results')
+# 	write_results(resources, write)
+
+# 	print('Finished.')
+
+
+def main(crawl_log, response_code, write=print):
+	# Store info as hash --> (occurences, urls)
+	resources = dict()
+	
+	print('Parse log file ("%s")...' % crawl_log)
+	with open(crawl_log, 'r') as src:
+		for line in src:
+			info = Info(line)
+			if omit_this(info, response_code):
+				continue # Omit any response code the user does not want
+			if info.hash in resources:
+				resources[info.hash][0] = resources[info.hash][0] + 1
+				resources[info.hash][1].add(info.url)
+			else:
+				resources[info.hash] = [1, set([info.url])]
+
+	print('Write results')
+
+	results = [resources[key] for key in resources]
+
+	for data in sorted(results, key=lambda result: -result[0]):
+		count  = data[0]
+		urls = data[1]
+		if count > 1:
+			first = True
+			for url in sorted(urls):
+				if first:
+					write('%d\t%s' % (count, url))
+					first = False
+				else:
+					write('\t%s' % url)
+			write()
+	print('Finished.')
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(prog = 'check-for-duplicates.py')
+	parser.add_argument('crawl_log')
+	parser.add_argument('-o', '--out', required=False)
+	parser.add_argument('-c', '--code', required=False, type=int, help='HTTP response code to filter before checking for duplicates.')
+	
+	options = parser.parse_args()
+
+	if options.out:
+		with open(options.out, 'w') as out:
+			def write(line=''):
+				out.write(line + '\n')
+			main(options.crawl_log, options.code, write)
+	else:
+		main(options.crawl_log, options.code, print)
---
+++ Scripts/look_for_missing_urls.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+from __future__ import print_function
+import argparse
+import sys
+
+
+def extract_url(line):
+	parts = line.split()
+	return parts[3]
+
+
+def extract_response_code(line):
+	parts = line.split()
+	return parts[1]
+
+
+def main(crawl_file_reference, crawl_file_compare, write):
+	'''
+	Given two crawler log files, this method looks which URLs where downloaded in the reference
+	(first log) to determine which one are missing in the second one.
+	'''
+	known_urls = set()
+	
+	print('Parse log file for comparison ("%s")...' % crawl_file_compare)
+	with open(crawl_file_compare, 'r') as src:
+		for line in src:
+			known_urls.add(extract_url(line))
+
+	missing_by_response_code = dict()
+
+	print('Parse reference log file ("%s") and compare urls...' % crawl_file_reference)
+	with open(crawl_file_reference, 'r') as src:
+		for line in src:
+			url = extract_url(line)
+			if url not in known_urls:
+				response_code = extract_response_code(line)
+				if response_code not in missing_by_response_code:
+					missing_by_response_code[response_code] = []
+				missing = missing_by_response_code[response_code]
+				missing.append(url)
+
+	print('Finished')
+
+
+	for response_code in sorted(missing_by_response_code):
+		write('Missing by response code %s:\n' % response_code)
+		for url in missing_by_response_code[response_code]:
+			write('\t' + url)
+		write('\n')
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(prog = 'look-for-missing-urls.py')
+	parser.add_argument('reference')
+	parser.add_argument('compare')
+	parser.add_argument('-o', '--out', required=False)
+	
+	options = parser.parse_args()
+
+	if options.out:
+		with open(options.out, 'w') as out:
+			def write(line=''):
+				out.write(line + '\n')
+			main(options.reference, options.compare, write)
+	else:
+		main(options.reference, options.compare, print)

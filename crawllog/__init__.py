6a3a2bc7a390785291329decf189717d25e8c74c
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2016-12-07 18:09:44 +0100

    Added context handling.

--- crawllog/__init__.py
+++ crawllog/__init__.py
@@ -6,6 +6,27 @@ import argparse
 import sys
 from crawllog.domains import domains
 
+
+class Context:
+    def __init__(self, args):
+        self.lines = open(args.logfile, 'r')
+        if args.out is not None:
+            self.dest = open(args.out, 'w')
+        else:
+            self.dest = sys.stdout
+
+    def write(self, *args):
+        print(*args, file=self.dest)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.lines.close()
+        if self.dest.close is not sys.stdout:
+            self.dest.close()
+
+
 def main():
     '''Setup API.'''
     parser = argparse.ArgumentParser()
@@ -15,7 +36,11 @@ def main():
 
     parser_domains = subparsers.add_parser('domains')
     parser_domains.add_argument('logfile', type=str, help='The crawl.log to process.')
+    parser_domains.add_argument('--code', type=int, default=None, help='Only count URLs for given response code.')
+    parser_domains.add_argument('--out', default=None, help='Only count URLs for given response code.')
     parser_domains.set_defaults(func=domains)
 
     args = parser.parse_args()
-    args.func(args)
+
+    with Context(args) as context:
+        args.func(args, context)
--- crawllog/domains.py
+++ crawllog/domains.py
@@ -1,14 +1,20 @@
+'''
+Creates a histogram of urls per domain.
+'''
+
 from crawllog.logfile import LogEntry
 
 
-def domains(args):
+def domains(args, context):
     '''Count URLs for domains'''
     urls = dict()
-    with open(args.logfile, 'r') as source:
-        for line in source:
-            log_entry = LogEntry(line)
-            domain = log_entry.domain()
-            if domain is not None:
+    for line in context.lines:
+        log_entry = LogEntry(line)
+        domain = log_entry.domain()
+        if domain is not None:
+            if domain not in urls:
+                urls[domain] = 0
+            if args.code is None or args.code is log_entry.response_code:
                 urls[domain] = urls.get(domain, 0) + 1
     for url in sorted(urls):
-        print(url, '\t', urls[url])
+        context.write(url, '\t', urls[url])
